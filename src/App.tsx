f00daa535a369f15726446f43ff7bc890276c63c
büşra erdem <busraerdmm994@gmail.com>
2026-02-17 11:33:08 +0300

    chore: initial project setup with Vite + React + TS

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+      <h1>Web Tasarimi ve Programlama</h1>
+      <h2>LAB-1</h2>
+      <p>Ad Soyad: [Busra Erdem]</p>
+      <p>Ogrenci No: [2305541022]</p>
+      </div>
+    </>
+  )
+}
+
+export default App
